062f9f392ae1b4c7395ffb6bf55e79b5d4250ea6
note_kdia <naruse147@gmail.com>
2024-05-09 00:25:13 +0900

    basic build settings for freestanding binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+fn efi_main() {
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
