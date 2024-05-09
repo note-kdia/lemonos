1ffece65761e4891eefa9bfc3964f1c5d32bca6e
note_kdia <naruse147@gmail.com>
2024-05-09 23:14:17 +0900

    boot from uefi?

---
+++ scripts/run_qemu.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+PATH_TO_EFI="$1"
+cd `dirname $0`
+cd ..
+mkdir -p mnt/EFI/BOOT
+cp ${PATH_TO_EFI} mnt/EFI/BOOT/BOOTX64.EFI
+qemu-system-x86_64 \
+    -bios third-party/ovmf/RELEASEX64_OVMF.fd \
+    -drive format=raw,file=fat:rw:mnt \
+    -monitor stdio
