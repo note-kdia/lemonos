88a21f04580a1392127300474e0dc56354d47dfc
Daiki Naruse <naruse147@gmail.com>
2024-06-03 23:18:48 +0900

    impl lib test handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-use self::efi::{EfiSimpleTextOutputProtocolWriter, EfiSystemTable};
+use crate::efi::{EfiSimpleTextOutputProtocolWriter, EfiSystemTable};
+use core::panic::PanicInfo;
 
 pub mod efi;
 pub mod error;
@@ -20,4 +24,56 @@ pub fn init(efi_system_table: &EfiSystemTable) {
         protocol: efi_system_table.con_out,
     };
     print::GLOBAL_WRITER.set_writer(efi_writer);
+}
+
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        use crate::{serial_print, serial_println};
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    use crate::qemu::{exit_qemu, ExitCode};
+    use crate::serial_println;
+
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(ExitCode::Success)
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    use crate::qemu::{exit_qemu, ExitCode};
+    use crate::serial_println;
+
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(ExitCode::Failed);
+
+    unreachable!();
+}
+
+#[cfg(test)]
+#[no_mangle]
+fn efi_main(_image_handle: crate::efi::EfiHandle, _efi_system_table: EfiSystemTable) {
+    test_main();
+    unreachable!();
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(lemonos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 use lemonos::efi::{EfiHandle, EfiSystemTable};
 use lemonos::println;
-use lemonos::serial_println;
 
 fn main() -> ! {
     println!("Enter main()");
@@ -32,50 +31,15 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: EfiSystemTable) {
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    use lemonos::serial_println;
     serial_println!("Error: {}\n", info);
     lemonos::x86_64::rest_in_peace();
-}
-
-pub trait Testable {
-    fn run(&self);
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        use lemonos::{serial_print, serial_println};
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    use lemonos::qemu::{exit_qemu, ExitCode};
-    use lemonos::serial_println;
-
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(ExitCode::Failed);
-
-    unreachable!();
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    use lemonos::qemu::{exit_qemu, ExitCode};
-    use lemonos::serial_println;
-
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(ExitCode::Success)
+    lemonos::test_panic_handler(info);
 }
 
 #[test_case]
